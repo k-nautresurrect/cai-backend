bc1b4d9407ff4b43d82134d0769f8c660453c7fb
k-nautresurrect <kushignite@gmail.com>
2021-11-28 15:59:17 +0530

    Added state of weather

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let city = 'meerut';
 let dataw;
 let lat;
 let lon;
-// let cord = {lattitude: 0, longitude: 0};
+let weath;
 
 
 app.get('/weather/:cityname',(req,res) => {
@@ -19,18 +19,20 @@ app.get('/weather/:cityname',(req,res) => {
             dataw = JSON.parse(data)
             lon = dataw.coord.lon;
             lat = dataw.coord.lat;
-            
+            weath = dataw.weather[0].main
+
+            console.log(weath);
             cord = {
                 lattitude: lat,
                 longitude: lon
             }
-            res.redirect(`/weather/${cord.lattitude}/${cord.longitude}`);
+            res.redirect(`/weather/${city}/${cord.lattitude}/${cord.longitude}/${weath}`);
         })
     })
 
 })
 
-app.get('/weather/:lat/:lon', (req,res) => {
+app.get('/weather/:city/:lat/:lon/:weath', (req,res) => {
     const urla = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${req.params.lat}&lon=${req.params.lon}&appid=${api_key}`
 
     https.get(urla, (resp) =>{
@@ -39,22 +41,18 @@ app.get('/weather/:lat/:lon', (req,res) => {
             const adata = JSON.parse(data);
             const listm = adata.list[0].main;
             const list = adata.list[0];
-            // const aqi = list.main.aqi
-            // console.log(list);
-            // console.log(aqi);
+         
             const para = {
                 aqi: listm.aqi,
                 pm2_5: list.components.pm2_5,
                 pm10: list.components.pm10
             }
 
-            // console.log(para.aqi);
-            res.send(`the aqi is ${para.aqi} and pm 2.5 is ${para.pm2_5}, pm10 is ${para.pm10}`);
+            
+            res.send(`the aqi of ${req.params.city} is ${para.aqi} and pm 2.5 is ${para.pm2_5}, pm10 is ${para.pm10} also the weather will be ${req.params.weath}`);
         })
     })
 })
-
-// const port = process.env.PORT || 3000
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log(`server started....`);
